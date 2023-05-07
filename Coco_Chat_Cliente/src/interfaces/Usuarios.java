a4ed4953a0ecf6e06f2b598dc21a895f290e94e9
NancyBan <nancyzendrero@gmail>
2023-05-07 03:22:26 -0600

    iconitos y menu ya medio hecho de los listados una vez iniciada la sesion

--- Coco_Chat_Cliente/src/interfaces/Usuarios.java
+++ Coco_Chat_Cliente/src/interfaces/Usuarios.java
@@ -23,8 +23,12 @@ public class Usuarios extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         jMenuBar1 = new javax.swing.JMenuBar();
         UsuariosMenu = new javax.swing.JMenu();
-        jMenu7 = new javax.swing.JMenu();
+        UsuariosConectadosMenu = new javax.swing.JMenuItem();
+        UsuariosDeconectadosMenu = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
+        AmigosConectadosMenu = new javax.swing.JMenuItem();
+        AmigosDesconectadosMenu = new javax.swing.JMenuItem();
+        jMenu7 = new javax.swing.JMenu();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -32,6 +36,7 @@ public class Usuarios extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        UsuariosMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/usuario.png"))); // NOI18N
         UsuariosMenu.setText("Usuarios");
         UsuariosMenu.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         UsuariosMenu.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
@@ -42,19 +47,53 @@ public class Usuarios extends javax.swing.JFrame {
                 UsuariosMenuActionPerformed(evt);
             }
         });
+
+        UsuariosConectadosMenu.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        UsuariosConectadosMenu.setText("Conectados");
+        UsuariosConectadosMenu.setMargin(new java.awt.Insets(2, 60, 2, 60));
+        UsuariosConectadosMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                UsuariosConectadosMenuActionPerformed(evt);
+            }
+        });
+        UsuariosMenu.add(UsuariosConectadosMenu);
+
+        UsuariosDeconectadosMenu.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        UsuariosDeconectadosMenu.setText("Desconectados");
+        UsuariosDeconectadosMenu.setMargin(new java.awt.Insets(2, 60, 2, 60));
+        UsuariosDeconectadosMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                UsuariosDeconectadosMenuActionPerformed(evt);
+            }
+        });
+        UsuariosMenu.add(UsuariosDeconectadosMenu);
+
         jMenuBar1.add(UsuariosMenu);
 
+        jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/amigos.png"))); // NOI18N
+        jMenu4.setText("Amigos");
+        jMenu4.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jMenu4.setIconTextGap(5);
+        jMenu4.setMargin(new java.awt.Insets(5, 90, 5, 90));
+
+        AmigosConectadosMenu.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        AmigosConectadosMenu.setText("Conectados");
+        AmigosConectadosMenu.setMargin(new java.awt.Insets(2, 60, 2, 60));
+        jMenu4.add(AmigosConectadosMenu);
+
+        AmigosDesconectadosMenu.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        AmigosDesconectadosMenu.setText("Desconectados");
+        AmigosDesconectadosMenu.setMargin(new java.awt.Insets(2, 60, 2, 60));
+        jMenu4.add(AmigosDesconectadosMenu);
+
+        jMenuBar1.add(jMenu4);
+
+        jMenu7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/grupos.png"))); // NOI18N
         jMenu7.setText("Grupos");
         jMenu7.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
         jMenu7.setIconTextGap(5);
         jMenu7.setMargin(new java.awt.Insets(5, 80, 5, 80));
         jMenuBar1.add(jMenu7);
-
-        jMenu4.setText("Amigos");
-        jMenu4.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        jMenu4.setIconTextGap(5);
-        jMenu4.setMargin(new java.awt.Insets(5, 90, 5, 90));
-        jMenuBar1.add(jMenu4);
 
         setJMenuBar(jMenuBar1);
 
@@ -73,11 +112,18 @@ public class Usuarios extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void UsuariosMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UsuariosMenuActionPerformed
-        //---------------------------
+       
+    }//GEN-LAST:event_UsuariosMenuActionPerformed
+
+    private void UsuariosDeconectadosMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UsuariosDeconectadosMenuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_UsuariosDeconectadosMenuActionPerformed
+
+    private void UsuariosConectadosMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UsuariosConectadosMenuActionPerformed
         Amigos a = new Amigos();
         a.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_UsuariosMenuActionPerformed
+    }//GEN-LAST:event_UsuariosConectadosMenuActionPerformed
 
     /**
      * @param args the command line arguments
@@ -116,6 +162,10 @@ public class Usuarios extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem AmigosConectadosMenu;
+    private javax.swing.JMenuItem AmigosDesconectadosMenu;
+    private javax.swing.JMenuItem UsuariosConectadosMenu;
+    private javax.swing.JMenuItem UsuariosDeconectadosMenu;
     private javax.swing.JMenu UsuariosMenu;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
