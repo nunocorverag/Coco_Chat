12f532ad1ba1c36ebb8bce4cdfc903a95c032639
gnuno2003 <gnuno2003@gmail.com>
2023-05-07 00:54:34 -0600

    Se manda respuesta del servidor al cliente en el login

--- Coco_Chat_Server/src/coco_chat_server/Coco_Chat_Server.java
+++ Coco_Chat_Server/src/coco_chat_server/Coco_Chat_Server.java
@@ -11,8 +11,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import coco_chat_server.HilosCliente;
 import db_conection_package.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Coco_Chat_Server {
 
@@ -20,6 +24,8 @@ public class Coco_Chat_Server {
         try {
             Socket clientSocket;
             ServerSocket serverSocket = new ServerSocket(1234);
+
+            Map<String, Integer> intentosInicioSesion = new HashMap<>();
             
             while(true)
             {
@@ -27,11 +33,17 @@ public class Coco_Chat_Server {
                clientSocket = serverSocket.accept();
                System.out.println("Conexion recibida");
                
-               InputStream inputStream = clientSocket.getInputStream();
-                byte[] buffer = new byte[1024];
-                int bytesRead = inputStream.read(buffer);
-                String funcion = new String(buffer, 0, bytesRead);
-               System.out.println(funcion);
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                String clienteConectado =  clientSocket.getInetAddress().getHostAddress();
+               
+                if(!(intentosInicioSesion.containsKey(clienteConectado)))
+                {
+                    intentosInicioSesion.put(clienteConectado, 0);
+                }
+                DataInputStream salidaFuncion = new DataInputStream(clientSocket.getInputStream());
+                String funcion = salidaFuncion.readUTF();
+                System.out.println("Funcion: " + funcion);
                
                if(funcion.equals("register"))
                {
@@ -58,13 +70,31 @@ public class Coco_Chat_Server {
                       if(usuarioDAO.IniciarSesion(loginDetails.username, loginDetails.password))
                       {
                           //chido
-                          System.out.println("Se logro");
-
+                          System.out.println("Credenciales validas");
+                          intentosInicioSesion.put(clienteConectado, 0);
+                          DataOutputStream respuestaCredencialesValidas = new DataOutputStream(clientSocket.getOutputStream());
+                          respuestaCredencialesValidas.writeUTF("credenciales_validas");
                       }
                       else
                       {
                           //chafa
-                          System.out.println("No se logro");
+                          System.out.println("Credenciales NO validas");
+                          int cantidad_intentos = intentosInicioSesion.get(clienteConectado);
+                          cantidad_intentos ++;
+                          System.out.println("Cantidad intentos: " + cantidad_intentos);
+                          intentosInicioSesion.put(clienteConectado, cantidad_intentos);
+                          if(cantidad_intentos == 3)
+                          {
+                            intentosInicioSesion.put(clienteConectado, 0);
+                            DataOutputStream respuestaRedirigir = new DataOutputStream(clientSocket.getOutputStream());
+                            respuestaRedirigir.writeUTF("redirigir");
+                          }
+                          else
+                          {
+                            DataOutputStream respuestaCredencialesNoValidas = new DataOutputStream(clientSocket.getOutputStream());
+                            respuestaCredencialesNoValidas.writeUTF("credenciales_invalidas");
+                          }
+
                       }   
                     } catch (ClassNotFoundException ex) {
                         Logger.getLogger(Coco_Chat_Server.class.getName()).log(Level.SEVERE, null, ex);
