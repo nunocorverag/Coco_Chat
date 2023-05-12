74ce8736b6be0eeb9e9f3c006ef01b10af6ee052
NancyBan <nancyzendrero@gmail>
2023-05-11 23:53:52 -0600

    interfaz de solicitudes de amistad

--- Coco_Chat_Cliente/src/interfaces/SolicitudesAmistad.java
+++ Coco_Chat_Cliente/src/interfaces/SolicitudesAmistad.java
@@ -4,6 +4,21 @@
  */
 package interfaces;
 
+import db_conection_package.Usuario;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import user_session.SessionManager;
+
 /**
  *
  * @author Nancy
@@ -11,10 +26,35 @@ package interfaces;
 public class SolicitudesAmistad extends javax.swing.JFrame {
 
     /**
-     * Creates new form SolicitudesAmistad
+     * Creates new form AgregarAmigos
      */
     public SolicitudesAmistad() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.addWindowListener(new WindowAdapter()
+                {
+                    public void windowClosing(WindowEvent e)
+                    {
+                        Socket s;      
+                        try {
+                            String direccionServidor = "10.147.17.147";
+                            InetAddress direccion = InetAddress.getByName(direccionServidor);
+                            s = new Socket(direccion, 1234);
+                            
+                            DataOutputStream funcion = new DataOutputStream(s.getOutputStream());
+                            funcion.writeUTF("cerrar_sesion");
+                            
+                            String username = SessionManager.getUsername();
+                            DataOutputStream objectOS = new DataOutputStream(s.getOutputStream());
+                            objectOS.writeUTF(username);
+                           
+                            s.close();
+                        } catch (IOException ex) {
+                            Logger.getLogger(InicioSesion.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+        );
     }
 
     /**
@@ -25,22 +65,127 @@ public class SolicitudesAmistad extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ListaUsuarios = new javax.swing.JList<>();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 15)); // NOI18N
+        jLabel1.setText("Solicitudes de amistad");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.weighty = 0.05;
+        getContentPane().add(jLabel1, gridBagConstraints);
+
+        ListaUsuarios.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        ListaAmigos AmigosList = new ListaAmigos();
+        ArrayList<Usuario> Amigos = AmigosList.obtenerAmigos();
+
+        DefaultListModel<String> modeloLista = new DefaultListModel<>();
+        for(Usuario amigo: Amigos)
+        {
+            modeloLista.addElement(amigo.username);
+        }
+        ListaUsuarios.setModel(modeloLista);
+        ListaUsuarios.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ListaUsuariosMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(ListaUsuarios);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jScrollPane1, gridBagConstraints);
+
+        jMenuBar1.setToolTipText("k");
+        jMenuBar1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jMenuBar1.setPreferredSize(new java.awt.Dimension(30, 30));
+
+        jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/PaAtras.png"))); // NOI18N
+        jMenu2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jMenu2.addMenuListener(new javax.swing.event.MenuListener() {
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+            }
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+                jMenu2MenuSelected(evt);
+            }
+        });
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    
+    public class ListaAmigos{
+        private ArrayList<Usuario> Amigos;
+        
+        public ListaAmigos()
+        {
+            this.Amigos = new ArrayList<Usuario>();
+            
+            Socket s;
+            try {
+                    String direccionServidor = "10.147.17.147";
+                    InetAddress direccion = InetAddress.getByName(direccionServidor);
+                    s = new Socket(direccion, 1234);
+                    
+                    DataOutputStream funcion = new DataOutputStream(s.getOutputStream());
+                    funcion.writeUTF("mostrar_no_amigos");
+                    
+                    String UserLogged = SessionManager.getUsername();
+                    DataOutputStream mandarUsername = new DataOutputStream(s.getOutputStream());
+                    mandarUsername.writeUTF(UserLogged);
+                    
+                    System.out.println("Se mando el user: " + UserLogged);
+                    
+                    ObjectInputStream AmigosObject = new ObjectInputStream(s.getInputStream());
+                    
+                    try {
+                        ArrayList<Usuario> Amigos = (ArrayList<Usuario>)AmigosObject.readObject();
+                        s.close();
+                        for(Usuario user: Amigos)
+                        {
+                            this.Amigos.add(user);
+                        }
+                  
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Usuarios.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+            }
+            catch (IOException ex) 
+            {
+                Logger.getLogger(Usuarios.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        public ArrayList<Usuario> obtenerAmigos()
+        {
+            return this.Amigos;
+        }
+    }
+    private void jMenu2MenuSelected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_jMenu2MenuSelected
+        Amigos a = new Amigos();
+        a.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jMenu2MenuSelected
+
+    private void ListaUsuariosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ListaUsuariosMouseClicked
+        String nombreSeleccionado = ListaUsuarios.getSelectedValue();
+        
+    }//GEN-LAST:event_ListaUsuariosMouseClicked
 
     /**
      * @param args the command line arguments
@@ -68,6 +213,13 @@ public class SolicitudesAmistad extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(SolicitudesAmistad.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -78,5 +230,10 @@ public class SolicitudesAmistad extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JList<String> ListaUsuarios;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
